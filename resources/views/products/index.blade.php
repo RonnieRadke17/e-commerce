877b09f36c3ff926eb66d512036935b1ab9c79ca
RonnieRadke17 <alexisemmanuelzombie@gmail.com>
2024-08-15 08:43:51 -0600

    commit antes de usar react(intento de implementacion)

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title','Productos')
+@section('title','Productos | E-commerce')
 @section('content')
 <main class="p-8 flex-grow relative">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-y-10">
